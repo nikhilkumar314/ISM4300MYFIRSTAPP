5d86c7dc54207f03f33a9b7ea9c71173bb81b50e
Nikhil Kumar <nikhilkumar@usf.edu>
2019-08-29 16:23:46 -0400

    <change>

--- ISM4300MYFIRSTAPP/Program.cs
+++ ISM4300MYFIRSTAPP/Program.cs
@@ -10,6 +10,9 @@ namespace ISM4300MYFIRSTAPP
     {
         static void Main(string[] args)
         {
+            //This is my first app
+            Console.WriteLine("This is my first console app");
+            Console.WriteLine("I am learning how to publish to GitHub"); 
         }
     }
 }
